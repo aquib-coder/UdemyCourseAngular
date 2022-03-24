ae7179c6526298ba8fe2574efa147ccd99c78b75
Aquib Javed <atif9087@gmail.com>
2022-03-24 19:57:55 +0530

    Again implemented last commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import { Component, OnInit, Output , EventEmitter} from '@angular/core';
-=======
 
 import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
->>>>>>> f3065404319d22acefa75399c3d3e4a8182452b1
 
 
 @Component({
@@ -12,22 +8,12 @@ import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-<<<<<<< HEAD
-@Output() OnFeatureSelected=new EventEmitter<string>();
-=======
   @Output () featureSelected=new EventEmitter<string>();
->>>>>>> f3065404319d22acefa75399c3d3e4a8182452b1
   constructor() { }
   ngOnInit(): void {
   }
-<<<<<<< HEAD
-  OnSelected(s:string){
-this.OnFeatureSelected.emit(s);
-  }
-=======
   OnSelect(feature:string){
     this.featureSelected.emit(feature);
   }
 
->>>>>>> f3065404319d22acefa75399c3d3e4a8182452b1
 }
